ee85634ce86a76da366a823a66dfcc3222c350ce
George Ibrahim Aziz <103423847+georgeibrahim1@users.noreply.github.com>
2026-01-24 00:36:41 +0200

    feat: add a modal screen to the noraml question display screen, add the logic of returning a right or wrong answer, add dynamic bindings too !

--- src/app.py
+++ src/app.py
@@ -2,8 +2,8 @@ import os
 from zandev_textual_widgets import FileSelector
 
 from textual.app import App, ComposeResult
-from textual.screen import Screen
-from textual.containers import Container, Horizontal
+from textual.screen import Screen , ModalScreen
+from textual.containers import Container, Horizontal, Grid
 from textual.widgets import Header, Footer, Label, DirectoryTree, Button, MarkdownViewer
 
 from helpers.parser import MdParser
@@ -14,7 +14,7 @@ class MemoApp(App):
     BINDINGS = []
     
     def __init__(self):
-        super.__init__()
+        super().__init__()
         self.truePerSession = [] 
         self.falsePerSession = [] # both will be erased if the user does another session after another session
 
@@ -59,27 +59,27 @@ class HelloScreen(Screen):
 
 class SessionClass(Screen):
     def __init__(self,md:str):
-        super.__init__()
+        super().__init__()
         self.md = md
-        self.cards = MdParser(self.md)
+        self.cards = MdParser(self.md).cards
         
     def on_mount(self):
-        self._push_questions_screens(self.cards) # add error validations
+        self._push_questions_screens() # add error validations
 
-    def _push_questions_screens(self,cards):
+    def _push_questions_screens(self):
 
-        n = len(cards)
+        n = len(self.cards)
 
         for x in range(n-1,-1,-1): # add from behind as it's like a stack
-            qt = self.app.get_question_screen_type(cards[x])
+            qt = self.app.get_question_screen_type(self.cards[x])
             if(qt == "FillInGaps"):
-                self.app.push_screen(Question_Display_FillInGaps(cards[x]))
+                self.app.push_screen(Question_Display_FillInGaps(self.cards[x]))
             elif(qt == "MultipleChoices"):
-                self.app.push_screen(Question_Display_MultipleChoices(cards[x]))
+                self.app.push_screen(Question_Display_MultipleChoices(self.cards[x]))
             elif(qt == "NormalorPhoto"):
-                self.app.push_screen(Question_Display_NormalorPhoto(cards[x]))
+                self.app.push_screen(Question_Display_NormalorPhoto(self.cards[x]))
             elif(qt == "Order"):
-                self.app.push_screen(Question_Display_Order(cards[x]))
+                self.app.push_screen(Question_Display_Order(self.cards[x]))
             else:
                 self.app.push_screen(Question_Display_Dummy())
 
@@ -105,12 +105,14 @@ class Question_Display_NormalorPhoto(Screen):
     CSS_PATH = "app.tcss"
 
     BINDINGS = [
-        ("s","skip","Skip")
+        ("s","skip","Skip"),
+        ("enter", "showModal", "Continue")
     ]
 
     def __init__(self,card):
         super().__init__()
         self.card = card
+        self.answer_shown = False
     
     def compose(self) -> ComposeResult:
         yield Header()
@@ -127,14 +129,62 @@ class Question_Display_NormalorPhoto(Screen):
         )
         yield Footer()
     
+    # thanks to https://textual.textualize.io/guide/actions/#__tabbed_4_1 (It took me like 3 hours to find a way to make the binding shit dynamic but it is like a gem in a fucking forest)
+    def check_action(self, action: str, parameters: tuple[object, ...]) -> bool | None:
+        """Check if an action may run."""
+        if action == "showModal" and not self.answer_shown:
+            return False 
+        if action == "skip" and self.answer_shown:
+            return False 
+        return True
 
     def on_button_pressed(self,event:Button.Pressed):
         if(event.button.id == 'answerButton'):
             self.get_widget_by_id("answerButton").add_class("hidden") # add class here with the widget class not the styles 
             self.get_widget_by_id("answerMarkDown").remove_class("hidden")
+            # self.BINDINGS.insert(1,("Enter" , "showModal" , "continue"))
+            # BINDINGS is constant, as it is only rendered or made on the mount of the screen, or edited by app.bind
+            self.answer_shown = True
+            self.refresh_bindings() 
+            
+
+    def action_showModal(self):
+
+        def check_answer(status:bool):
+            if status:
+                self.app.truePerSession.append(self.card)
+            else:
+                self.app.falsePerSession.append(self.card)
+
+            self.app.pop_screen()
+
+        self.app.push_screen(IsTrueOrFalseModalScreen() , check_answer)
 
     def action_skip(self):
         self.app.pop_screen()
+
+
+class IsTrueOrFalseModalScreen(ModalScreen[bool]):
+    def compose(self) -> ComposeResult:
+        yield Grid(
+            Label(
+                "Did you answer this question right?" , id="isTrueOrFalseModa_QuestionLabel"
+            ),
+            Button(
+                "Yes", variant="success", id="isTrueOrFalseModal_YesButton"
+            ),
+            Button(
+                "No" , variant="error",id="isTrueOrFalseModal_NoButton"
+            ),
+            id="isTrueOrFalseModal"
+        )
+    
+    def on_button_pressed(self, event:Button.Pressed):
+        if(event.button.id == "isTrueOrFalseModal_YesButton"):
+            self.dismiss(True) # pop and return a bool to the questionDisplay Screen
+        elif(event.button.id == "isTrueOrFalseModal_NoButton"):
+            self.dismiss(False) 
+
 
 class Question_Display_MultipleChoices(Screen):
 
